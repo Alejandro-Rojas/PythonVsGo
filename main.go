7a3b0875328bbe7f38c817e944917e54d9d1bd6d
alejandro-rojas <rojas.alejandro303@gmail.com>
2021-11-29 15:45:16 -0700

    u

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+//Buuble Sort
 func BubbleSort(array []int) []int {
 	start_time := time.Now()
 	for i := 0; i < len(array)-1; i++ {
@@ -14,9 +15,8 @@ func BubbleSort(array []int) []int {
 				array[j], array[j+1] = array[j+1], array[j]
 				end_time := time.Now()
 				elapsed := end_time.Sub(start_time)
-				//fmt.Println(elapsed.Seconds(), array)
-				fmt.Println(array)
-				fmt.Println(elapsed)
+				d2 := time.Duration(elapsed)
+				fmt.Println("time: ", d2)
 			}
 
 		}
@@ -24,21 +24,50 @@ func BubbleSort(array []int) []int {
 	return array
 }
 
-func BubbleSort2(array2 []int) []int {
-	start_time := time.Now()
-	for i := 0; i < len(array2)-1; i++ {
-		for j := 0; j < len(array2)-i-1; j++ {
-			if array2[j] > array2[j+1] {
-				array2[j], array2[j+1] = array2[j+1], array2[j]
-				end_time := time.Now()
-				elapsed := end_time.Sub(start_time)
-				//fmt.Println(elapsed.Seconds(), array)
-				fmt.Println(array2)
-				fmt.Print(elapsed)
-			}
+// helper to find large number in the Radix Sort
+func largestNum(array2 []int) int {
+	largestNum := 0
 
+	for i := 0; i < len(array2); i++ {
+		if array2[i] > largestNum {
+			largestNum = array2[i]
 		}
 	}
+	return largestNum
+}
+
+// Radix Sort
+func radixSort(array2 []int) []int {
+	start_time := time.Now()
+	largestNum := largestNum(array2)
+	size := len(array2)
+	significantDigit := 1
+	semiSorted := make([]int, size)
+	for largestNum/significantDigit > 0 {
+		bucket := [10]int{0}
+		for i := 0; i < size; i++ {
+			bucket[(array2[i]/significantDigit)%10]++
+		}
+		for i := 1; i < 10; i++ {
+			bucket[i] += bucket[i-1]
+		}
+		for i := size - 1; i >= 0; i-- {
+			bucket[(array2[i]/significantDigit)%10]--
+			semiSorted[bucket[(array2[i]/significantDigit)%10]] = array2[i]
+		}
+		for i := 0; i < size; i++ {
+			array2[i] = semiSorted[i]
+		}
+
+		significantDigit *= 10
+		end_time := time.Now()
+		elapsed := end_time.Sub(start_time)
+		d2 := time.Duration(elapsed)
+		//fmt.Println("\tSorting: "+strconv.Itoa(significantDigit)+"'s place", array2)
+		fmt.Println("radixSort time: ", d2)
+
+	}
+
 	return array2
 }
 
@@ -62,8 +91,7 @@ func main() {
 	rand.Shuffle(len(array), func(i, j int) { array[i], array[j] = array[j], array[i] })
 	fmt.Println("bubbleSort time:", BubbleSort(array))
 	println("-----------------------------------------------------------------------------------------")
-	println("Reverse List")
-	array2 := RangeInt(5000, 1, 10)
+	array2 := RangeInt(0, 5000, 10)
 	rand.Shuffle(len(array2), func(i, j int) { array[i], array[j] = array[j], array[i] })
-	fmt.Println("bubbleSort time:", BubbleSort2(array))
+	fmt.Println("radixSort time seconds: ", radixSort(array2))
 }
